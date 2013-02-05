36a89f8ca79431d39e76189cc83db0f91f54a325
Christian Helmich <kagekirin@gmail.com>
2013-02-05 18:45:10 +0900

    added AManagerSingleton<T>
    this is my version of the manager singleton

---
+++ Scripts/General/AManager/AManagerSingleton.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System;
+
+
+public abstract class AManagerSingleton<T> : MonoBehaviour where T: class
+{		
+	static public T Instance
+	{
+		get
+		{
+			return SingletonInternal.instance;
+		}
+
+		private set
+		{
+			SingletonInternal.instance = value;
+		}
+	}
+	static public T GetInstance()
+	{
+		return Instance;
+	}
+
+	private class SingletonInternal
+	{
+		static SingletonInternal()
+		{
+		}
+
+		static T FindOrCreateInstance()
+		{
+			T inst = FindObjectOfType(typeof(T)) as T;
+			if (inst == null)
+			{
+				GameObject obj = new GameObject(typeof(T).ToString());
+				inst = obj.AddComponent(typeof(T)) as T;
+				Debug.Log("could not locate object of type " + typeof(T).ToString() + " therefore created one");
+			}
+			return inst;
+		}
+
+		internal static T instance = FindOrCreateInstance();
+	};
+
+	void OnApplicationQuit()
+	{
+		Instance = null;
+	}
+};
